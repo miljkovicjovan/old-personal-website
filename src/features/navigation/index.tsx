654408afe9b56c2f92aa11af6f930b0fa177ca4c
Miljković Jovan <77690201+miljkovicjovan@users.noreply.github.com>
2024-02-09 17:45:26 +0100

    fixed nav and responsive

--- src/features/navigation/index.tsx
+++ src/features/navigation/index.tsx
@@ -1,21 +1,18 @@
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Navbar, Container, Nav } from "react-bootstrap";
 
 function Navigation() {
     return (
-        <Navbar expand="lg" className="bg-dark p-0">
-            <Container className="p-0 ps-5 py-4 justify-content-between">
-                <Navbar.Brand className="link-underline text-light fs-4" href="#home">miljkovicjovan.com</Navbar.Brand>
-                <Nav className="fs-4">
-                    <Nav.Link className="link-underline text-light" href="#about">About</Nav.Link>
-                    <Nav.Link className="link-underline text-light ms-5" href="#projects">Projects</Nav.Link>
-                    <Nav.Link className="link-underline text-light mx-5" href="#blog">Blog</Nav.Link>
-                    <span className="d-flex border-start border-end fs-4">
-                        <Nav.Link><FontAwesomeIcon className="icon-hover ms-5" icon={faLinkedin}/></Nav.Link>
-                        <Nav.Link><FontAwesomeIcon className="icon-hover mx-5" icon={faGithub}/></Nav.Link>
-                    </span>                        
-                </Nav>
+        <Navbar expand="lg" className="navbar-dark bg-dark p-0">
+            <Container className="p-0 ps-5 py-4">
+                <Navbar.Brand className="link-underline text-light fs-4 me-5" href="#home">miljkovicjovan.com</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="fs-5">
+                        <Nav.Link className="link-underline text-light" href="#about">About</Nav.Link>
+                        <Nav.Link className="link-underline text-light" href="#projects">Projects</Nav.Link>
+                        <Nav.Link className="link-underline text-light" href="#blog">Blog</Nav.Link>                      
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
