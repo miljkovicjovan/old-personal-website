774c9cef7ca9afee14046a7e4ce185b6c6a149b8
Miljković Jovan <77690201+miljkovicjovan@users.noreply.github.com>
2024-02-09 17:49:02 +0100

    fixed the spacing & changed brand

--- src/features/navigation/index.tsx
+++ src/features/navigation/index.tsx
@@ -4,12 +4,12 @@ function Navigation() {
     return (
         <Navbar expand="lg" className="navbar-dark bg-dark p-0">
             <Container className="p-0 ps-5 py-4">
-                <Navbar.Brand className="link-underline text-light fs-4 me-5" href="#home">miljkovicjovan.com</Navbar.Brand>
+                <Navbar.Brand className="link-underline text-light fs-4 me-5" href="#home">Miljkovic Jovan</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="fs-5">
                         <Nav.Link className="link-underline text-light" href="#about">About</Nav.Link>
-                        <Nav.Link className="link-underline text-light" href="#projects">Projects</Nav.Link>
+                        <Nav.Link className="link-underline text-light mx-0 mx-lg-4" href="#projects">Projects</Nav.Link>
                         <Nav.Link className="link-underline text-light" href="#blog">Blog</Nav.Link>                      
                     </Nav>
                 </Navbar.Collapse>
