98809e97f0aca792390982b80436be3bac41d973
Miljković Jovan <77690201+miljkovicjovan@users.noreply.github.com>
2024-02-11 00:35:14 +0100

    fixed dark bg

--- src/features/about/index.tsx
+++ src/features/about/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-scroll";
 
 function About() {
     return (
-        <Stack id="about" className="mt-5 px-5 flex-lg-row justify-content-around">
+        <Stack id="about" className="bg-dark text-light py-5 px-5 flex-lg-row justify-content-around">
             <Stack className="px-0 px-sm-5 col-lg-3 col-12">
                 <img width="200" height="200" className="rounded-circle mb-3" 
                 src="https://avatars.githubusercontent.com/u/77690201?v=4" alt="Profile of Jovan Miljkovic"/>
@@ -13,10 +13,10 @@ function About() {
             <Stack className="px-0 px-sm-5 col-lg-6 col-12 mt-5 mt-lg-0 align-self-center">
                 <h1>Learner, developer, and enthusiast immersed in the realms of both timeless and cutting-edge technologies.</h1>
                 <Stack gap={2} className="flex-sm-row">
-                    <Button variant="dark">
+                    <Button className="border-light" variant="dark">
                         <Link to="projects" spy={true} smooth={true} duration={500}>My Work</Link>
                     </Button>
-                    <Button variant="dark">
+                    <Button className="border-light" variant="dark">
                         <Link to="blog" spy={true} smooth={true} duration={500}>My Blog</Link>
                     </Button>
                     <Button variant="secondary">
