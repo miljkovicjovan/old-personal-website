0cc8fdeeb14da02ee6e8f39f8e1e7a8867a896cb
Miljković Jovan <77690201+miljkovicjovan@users.noreply.github.com>
2024-02-11 13:19:26 +0100

    code cleanup

--- src/features/contact/index.tsx
+++ src/features/contact/index.tsx
@@ -1,5 +1,4 @@
-import { faGithub, faInstagram, faInstagramSquare, faLinkedin, faXTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faX } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faInstagram, faLinkedin, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Stack } from "react-bootstrap";
 
@@ -8,10 +7,10 @@ export default function Contact() {
         <Stack id="contact" className="py-5 bg-dark text-light px-5 justify-content-around align-items-center">
             <h1>Contact Me</h1>
             <Stack direction="horizontal" gap={5} className="align-self-center mt-2">
-                <a href="https://www.linkedin.com/in/miljkovicj/" target="_blank"><FontAwesomeIcon color="#0072b1" size="2x" icon={faLinkedin} /></a>
-                <a href="https://github.com/miljkovicjovan" target="_blank"><FontAwesomeIcon color="#967bb6" size="2x" icon={faGithub} /></a>
-                <a href="https://twitter.com/miljkoviccjovan" target="_blank"><FontAwesomeIcon color="#1DA1F2" size="2x" icon={faXTwitter} /></a>
-                <a href="https://www.instagram.com/miljkovicjovan/" target="_blank"><FontAwesomeIcon color="#833AB4" size="2x" icon={faInstagram} /></a>
+                <a href="https://www.linkedin.com/in/miljkovicj/" target="_blank" rel="noreferrer"><FontAwesomeIcon color="#0072b1" size="2x" icon={faLinkedin} /></a>
+                <a href="https://github.com/miljkovicjovan" target="_blank" rel="noreferrer"><FontAwesomeIcon color="#967bb6" size="2x" icon={faGithub} /></a>
+                <a href="https://twitter.com/miljkoviccjovan" target="_blank" rel="noreferrer"><FontAwesomeIcon color="#1DA1F2" size="2x" icon={faXTwitter} /></a>
+                <a href="https://www.instagram.com/miljkovicjovan/" target="_blank" rel="noreferrer"><FontAwesomeIcon color="#833AB4" size="2x" icon={faInstagram} /></a>
             </Stack>
         </Stack>
     );
--- src/features/wip/index.tsx
+++ src/features/wip/index.tsx
@@ -4,7 +4,7 @@ export default function WorkInProgress() {
     return (
         <Stack id="wip" className="py-5 bg-dark text-light align-items-center">
             <h3 className="text-decoration-underline">Website under development...</h3>
-            <iframe src="https://giphy.com/embed/2AF5KbIaGVA36ZkiAm" width="240" height="170"></iframe>
+            <iframe src="https://giphy.com/embed/2AF5KbIaGVA36ZkiAm" width="240" height="170" title="Website under development..."></iframe>
         </Stack>
     );
 }
