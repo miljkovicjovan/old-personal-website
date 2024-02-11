42700d4bcdb4246bf4a175810b3ba772743d397e
Miljković Jovan <77690201+miljkovicjovan@users.noreply.github.com>
2024-02-11 13:09:00 +0100

    added contact feature

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import Navigation from "../features/navigation";
 import About from "../features/about";
 import Experience from "../features/experience";
 import WorkInProgress from "../features/wip";
+import Contact from "../features/contact";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <About/>
       <Experience/>
       <WorkInProgress/>
+      <Contact/>
     </>
   );
 }
