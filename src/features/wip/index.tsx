6daf25d0f84fd2ce0f212e665e150afdad257117
Miljković Jovan <77690201+miljkovicjovan@users.noreply.github.com>
2024-02-11 12:43:20 +0100

    code cleanup wip feature

--- src/features/wip/index.tsx
+++ src/features/wip/index.tsx
@@ -4,7 +4,7 @@ export default function WorkInProgress() {
     return (
         <Stack className="py-5 bg-dark text-light align-items-center">
             <h1 className="text-decoration-underline">Website under development...</h1>
-            <iframe src="https://giphy.com/embed/2AF5KbIaGVA36ZkiAm" width="480" height="341" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+            <iframe src="https://giphy.com/embed/2AF5KbIaGVA36ZkiAm" width="480" height="341"></iframe>
         </Stack>
     );
 }
