6d829f31b830072beca94ac85aa6f038b5646987
Jovan Miljkovic <jovan@Jovans-MacBook-Air.local>
2024-10-06 23:58:33 +0200

    fixed work experience

--- src/features/experience/index.tsx
+++ src/features/experience/index.tsx
@@ -6,12 +6,14 @@ export default function Experience() {
         <Stack className="py-5 bg-dark text-light px-5 flex-xl-row justify-content-around">
             <Stack className="px-0 px-md-5 col-xl-6 col-12">
                 <h2 className="m-0">Work Experience</h2>
-                <Slot title="Full-Stack Developer & Product Owner" place="Adera" start="Jan 2022" end="Present"
-                img="https://media.licdn.com/dms/image/C4D0BAQF9VPE6kvzbsA/company-logo_200_200/0/1631357619540/aderaas_logo?e=1715817600&v=beta&t=eYckjM_qcKL3Xvjc11E39zER0Wbxa3ajmNKUzLZ1OdY"/>
+                <Slot title="Full-Stack Developer & Product Owner" place="Adera (Oslo, Norway)" start="Feb 2024" end="May 2024"
+                img="https://media.licdn.com/dms/image/v2/C4D0BAQF9VPE6kvzbsA/company-logo_200_200/company-logo_200_200/0/1631357619540/aderaas_logo?e=1736380800&v=beta&t=d6ocuyEeVgwT_k2HG1zV8vwGYzT-N4Z2erSTBJE5a0M"/>
+                <Slot title="Full-Stack Developer" place="Adera (Oslo, Norway)" start="Jan 2023" end="Feb 2024"
+                img="https://media.licdn.com/dms/image/v2/C4D0BAQF9VPE6kvzbsA/company-logo_200_200/company-logo_200_200/0/1631357619540/aderaas_logo?e=1736380800&v=beta&t=d6ocuyEeVgwT_k2HG1zV8vwGYzT-N4Z2erSTBJE5a0M"/>
             </Stack>
             <Stack className="px-0 px-md-5 mt-5 mt-xl-0 col-xl-6 col-12">
                 <h2 className="m-0">Education</h2>
-                <Slot title="Bachelor's of Software and Data Engineering" place="Singidunum University, Belgrade" start="2020" end="2024"
+                <Slot title="Bachelor's of Software and Data Engineering" place="Singidunum University (Belgrade, Serbia)" start="2020" end="2024"
                 img="https://pbs.twimg.com/profile_images/1267809191/SINGIDUNUM_400x400.png"/>
             </Stack>
         </Stack>
