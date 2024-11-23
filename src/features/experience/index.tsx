49ada91f25eeded0148b7f79839a7d2bbb67d6e5
Miljković Jovan <77690201+miljkovicjovan@users.noreply.github.com>
2024-11-23 17:37:16 +0100

    added new job position & added icon work image fallback

--- src/features/experience/index.tsx
+++ src/features/experience/index.tsx
@@ -6,9 +6,10 @@ export default function Experience() {
         <Stack className="py-5 bg-dark text-light px-5 flex-xl-row justify-content-around">
             <Stack className="px-0 px-md-5 col-xl-6 col-12">
                 <h2 className="m-0">Work Experience</h2>
+                <Slot title="Mobile App Developer (Flutter & Firebase)" place="DinDin (Belgrade, Serbia)" start="Sep 2024" end="Current"/>
                 <Slot title="Full-Stack Developer & Product Owner" place="Adera (Oslo, Norway)" start="Feb 2024" end="May 2024"
                 img="https://media.licdn.com/dms/image/v2/C4D0BAQF9VPE6kvzbsA/company-logo_200_200/company-logo_200_200/0/1631357619540/aderaas_logo?e=1736380800&v=beta&t=d6ocuyEeVgwT_k2HG1zV8vwGYzT-N4Z2erSTBJE5a0M"/>
-                <Slot title="Full-Stack Developer" place="Adera (Oslo, Norway)" start="Jan 2023" end="Feb 2024"
+                <Slot title="Full-Stack Developer (React & .NET)" place="Adera (Oslo, Norway)" start="Jan 2023" end="Feb 2024"
                 img="https://media.licdn.com/dms/image/v2/C4D0BAQF9VPE6kvzbsA/company-logo_200_200/company-logo_200_200/0/1631357619540/aderaas_logo?e=1736380800&v=beta&t=d6ocuyEeVgwT_k2HG1zV8vwGYzT-N4Z2erSTBJE5a0M"/>
             </Stack>
             <Stack className="px-0 px-md-5 mt-5 mt-xl-0 col-xl-6 col-12">
--- src/features/experience/slot.tsx
+++ src/features/experience/slot.tsx
@@ -1,3 +1,5 @@
+import { faBuildingUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Stack } from "react-bootstrap";
 
 type info = {
@@ -5,14 +7,20 @@ type info = {
     place: string, 
     start: string, 
     end: string,
-    img: string
+    img?: string
 }
 
 export default function Slot({title, place, start, end, img}: info) {
     return (
         <Stack direction="horizontal" className="my-3 p-1 py-3 px-md-4 border border-light rounded">
-            <img width="40" height="40" className="rounded-circle align-self-center ms-md-0 ms-3 me-3" 
-                src={img} alt="Profile of Jovan Miljkovic"/>
+            {img ? (<img width="40" height="40" 
+                    className="rounded-circle align-self-center ms-md-0 ms-3 me-3" 
+                    src={img} alt="Profile of Jovan Miljkovic"
+            />): <FontAwesomeIcon 
+                icon={faBuildingUser} 
+                className="align-self-center ms-md-0 ms-4 me-3" 
+                size="xl"
+            />}
             <Stack className="flex-md-row">
                 <Stack>
                     <h5>{title}</h5>
